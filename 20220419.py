de6176ff7f79dc21ecef1406731a1ae16cd8f6d5
叶丽霞 <yelixia@corp.netease.com>
2022-04-21 20:01:13 +0800

    20220421学习ptyhon

--- 20220419.py
+++ 20220419.py
@@ -1,9 +1,25 @@
 
 def test1111():
-    pass
+    str_a="studyPython"
+    print(str_a[1:])
+    print(str_a[1::])
+    print(str_a[1:5])
+    print(str_a[1:5:2])
+    print(str_a[1:7:2])
+    print(str_a[1:-1])
+    print(str_a[::])
+    list_a=[1,2,3,4,5,6,7]
+    print(list_a[1:])
+    print(list_a[1::])
+    print(list_a[1:-1])
+
+
 
 def test222():
     pass
 
 def testdev():
     pass
+
+if __name__ == '__main__':
+    test1111()
