6cfacc58684737dcab51b26afbc40e3b70e6f2c7
叶丽霞 <yelixia@corp.netease.com>
2022-04-24 14:18:38 +0800

    添加字符串类型操作

--- 20220419.py
+++ 20220419.py
@@ -14,12 +14,26 @@ def test1111():
     print(list_a[1:-1])
 
 
-
+#字符串基本操作
 def test222():
-    pass
+    print(("this is str %s" % "string1"))
+    print(("this is str %d" % 111))
+    print(("this is str %f" % 111.3))
+    print(("this is format_string {} {} {}").format("yes", "or","no"))
+    print(("this is format_string {2} {1} {0}").format("yes", "or","no"))
+
+    print(("this is format_string {yes} {or_1} {no}").format(yes="yes", or_1="or",no="no"))
+    name="sandy"
+    print(f"my name is {name}")
+    str1="|it is very good|"
+    list1= str1.split(" ")
+    print(list1)
+    print("@".join(list1))
+    print(str1.strip("|"))
+    print(str1.replace("good", "bad"))
 
 def testdev():
     pass
 
 if __name__ == '__main__':
-    test1111()
+    test222()
